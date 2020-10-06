9f219a3cc1da443e08334fb2fd87f8fad21344ae
kioria95 <kenkioria@gmail.com>
2020-10-06 15:47:25 +0300

    Added Feed and Widgets

---
+++ src/SidebarRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SidebarRow.css'
+
+function SidebarRow({title}) {
+    return (
+        <div className="sidebarRow">
+            <p>{title}</p>
+        </div>
+    )
+}
+
+export default SidebarRow
